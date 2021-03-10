a82189c53bc0e008e1ea96623d5fe74327c57dbe
Corey Mckeel <coreyt.mckeel@gmail.com>
2021-03-09 19:13:01 -0500

    Added projectiles and worldbuilding scripts

---
+++ Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour
+{
+    public Rigidbody Projectile;
+    public float Speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody p = Instantiate(Projectile, transform.position, transform.rotation);
+            p.velocity = transform.forward * Speed;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public GameObject block;
+    public int width = 1000;
+    public int height = 1000;
+    
+
+    void Start()
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+}
